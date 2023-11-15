b6ee50498b923f9baf808143d5a4de8a18f3f1a4
THAO DUONG TRUNG <MISA_DEVELOP\dtthao@dtthao-ub.misa-group.vn>
2023-11-15 12:56:44 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <div class="row container">
+    <!-- <div class="column">
+      <card-item v-for="item in data" :key="content" :content="item.content"></card-item>
+    </div> -->
+
+    <div class="column">
+      <draggable v-model="data" group="people" @start="drag = true" @end="drag = false" item-key="content">
+        <template #item="{ element }">
+          <card-item :content="element.content"></card-item>
+        </template>
+      </draggable>
+    </div>
+
+    <div class="column">
+      <draggable v-model="data" group="people" @start="drag = true" @end="drag = false" item-key="content">
+        <template #item="{ element }">
+          <card-item :content="element.content"></card-item>
+        </template>
+      </draggable>
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ducimus a explicabo tenetur distinctio eum odit et
+      nobis. Cumque facilis, corporis excepturi odit praesentium saepe et autem recusandae veritatis quibusdam?</p>
+    <!-- <div class="column">
+      <card-item v-for="item in data" :key="content" :content="item.content"></card-item>
+    </div> -->
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import CardItem from './components/CardItem.vue'
+import draggable from 'vuedraggable'
+const drag = ref(false);
+const data = ref([{ content: 'Item 1', },
+{ content: 'Item 2', },
+{ content: 'Item 3', },
+{ content: 'Item 4', },
+{ content: 'Item 5', },
+{ content: 'Item 6', },
+{ content: 'Item 7', },
+{ content: 'Item 8', },
+{ content: 'Item 9', },
+{ content: 'Item 10  ', }])
+
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+  background-color: gray;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+  justify-content: center;
+  align-items: center;
+  background-color: gray;
+}
+</style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="card-item">{{ content }}</div>
+</template>
+
+<script setup>
+const props = defineProps(['content']);
+</script>
+
+<style scoped>
+.card-item {
+    width: 50px;
+    height: 50px;
+    background-color: #fff;
+    text-align: center;
+    vertical-align: middle;
+    border: 1px solid #000;
+    margin-top: 20px;
+}
+</style>
